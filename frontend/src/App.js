3968c8d491b4260d50e9c7d2d7f4472f6fd936a8
Mr-RK-11 <rohithkumarh1156@gmail.com>
2026-04-09 11:10:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import SignIn from './pages/SignIn';
-import Survey from './pages/Survey';
-import Upload from './pages/Upload';
-import Dashboard from './pages/Dashboard';
-import Chat from './pages/Chat';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/survey" element={<Survey />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/dashboard/:docId" element={<Dashboard />} />
-          <Route path="/chat/:docId" element={<Chat />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+    <div style={{ padding: 20 }}>
+      <h1>Frontend Working 🚀</h1>
     </div>
   );
 }
