71bbbc82616ee337e129176d7aaf650b85069002
Mr-RK-11 <rohithkumarh1156@gmail.com>
2026-04-09 07:40:56 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -17,9 +17,6 @@ import io
 import base64
 from urllib.parse import quote
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
-
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
